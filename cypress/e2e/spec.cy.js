30ba0c4cc714ded127de451135bb293b8e39fc28
João Victor da Silva <jooao_1999@hotmail.com>
2023-05-25 16:05:43 -0300

    att senha

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,26 @@
+
+/// <reference types="cypress" />
+
+describe('RFs', () => {
+
+  it('Realizar Login', () => {
+
+    // cy.get - Busca de elemento
+    // .type  - Insere um texto
+
+    //cy.visit('https://plataforma.xglobal.com.br/login')
+
+    cy.get('#username').type('robo.rf')
+
+    cy.get('#password').type('@Xglobal23')
+
+    cy.get('.MuiButtonBase-root').click()
+
+    //cy.get('href="/cadastros"]').click()
+  })
+
+  it('Realizar Cadastro do RF', () => {
+
+  })
+
+})
